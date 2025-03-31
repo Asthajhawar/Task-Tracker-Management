78f6f7f10820d5e1b5c55e5df40dd380645b324f
AsthaJhawar <asthajhawar@gmail.com>
2025-03-31 16:45:52 +0530

    Initial commit

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,51 @@
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  Output,
+  signal,
+} from '@angular/core';
+import { User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
+
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+// interface User {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+  @Output() select = new EventEmitter();
+
+  get imagePath() {
+    return 'assets/users/' + this.user.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+
+  // // Using SIGNAL
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+}
